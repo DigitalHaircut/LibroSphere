e3787e159f6f237badb1c69bb955208e8426a44c
DigitalHaircut <andr.oprea@gmail.com>
2023-04-21 20:45:45 +0300

    Typo corrected

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,21 @@
-import { Box, Typography, Link } from "@mui/material";
-import { Navlink } from "react-router-dom";
 
-export default function () {
+import { Box, Typography, Link, TextField } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+export default function SignIn() {
   return (
-    <Box class="flexCenter">
+    <Box className="flexCenter" sx={{ mt: 4 }}>
       <Typography variant="h5"> Sign in </Typography>
       <Typography variant="body1">
         or{" "}
-        <Link component={Navlink} to="/">
+        <Link component={NavLink} to="/">
           explore the app
         </Link>
       </Typography>
+      <Box component="form" sx={{ mt: 2 }}>
+        <TextField label="Email" fullWidth margin="normal" />
+        <TextField label="Password" fullWidth margin="normal" />
+      </Box>
     </Box>
   );
 }
