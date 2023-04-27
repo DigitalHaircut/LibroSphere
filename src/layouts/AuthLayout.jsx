87175ada6cd13c684495ef365b00e79d145eb3cf
DigitalHaircut <andr.oprea@gmail.com>
2023-04-27 17:52:19 +0300

    Update AuthLayout.jsx

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -2,7 +2,7 @@ import { Box, Container } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { Header } from "../components/Header";
 
-export function AppLayout() {
+export default function AuthLayout() {
 return (
     <Box>
         <Header />
