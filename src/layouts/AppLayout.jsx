6d396ff8f06d748ba19422373e91091807223209
DigitalHaircut <andr.oprea@gmail.com>
2023-04-27 17:45:11 +0300

    Update AppLayout.jsx

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,5 +1,6 @@
 import { Box, Container } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import { Header } from "../components/Header";
 
 export function AppLayout() {
     return (
